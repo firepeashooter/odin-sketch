89a55ca26dcd176fe099b8d5326989e135ad9266
Ben Braun <benjamin.braun2003@gmail.com>
2025-06-11 18:55:29 -0400

    Colour any div orange when hovered over

--- code.js
+++ code.js
@@ -11,11 +11,27 @@ function createGrid(){
 
         newDiv.classList.add("cube");
         containerDiv.appendChild(newDiv);
-
-
-
     };
 
 }
 
 createGrid();
+
+//Creating the Hover Effect
+containerDiv = document.querySelector(".container");
+cubes = document.querySelectorAll(".cube");
+
+//Event Delegation
+
+containerDiv.addEventListener("mouseover", (e) => {
+    if (e.target.classList.contains("cube")){
+        e.target.classList.add("coloured");
+    }
+});
+
+containerDiv.addEventListener("mouseout", (e) => {
+    if (e.target.classList.contains("cube")){
+        e.target.classList.remove("coloured");
+    }
+
+})
