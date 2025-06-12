c1c03a8ce4c48881194ddd37b713834bf5147acc
Ben Braun <benjamin.braun2003@gmail.com>
2025-06-11 21:35:29 -0400

    Allow user to regenerate a new grid with given dimensions

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 
 
-//Create a 16x16 grid of divs
+//Create a size x size grid
 //Size in an int that specifies the size of the grid (it's a square)
 
 function createGrid(size){
@@ -17,10 +17,9 @@ function createGrid(size){
 
         for (let j = 0; j < size; j++){ //Create the cubes within the rows
 
-
             const newDivCube = document.createElement("div");
-            newDivCube.classList.add("cube");
 
+            newDivCube.classList.add("cube");
             newDivRow.appendChild(newDivCube);
         }
 
@@ -28,7 +27,18 @@ function createGrid(size){
 
 }
 
+//A function to delete the entire grid
+
+function deleteGrid(container){
+
+    //loop through the rows and just delete them
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+}
+
 createGrid(50);
+
 
 //Creating the Hover Effect
 const containerDiv = document.querySelector(".container");
@@ -39,7 +49,25 @@ const cubes = document.querySelectorAll(".cube");
 containerDiv.addEventListener("mouseover", (e) => {
     if (e.target.classList.contains("cube")){
         e.target.classList.add("coloured");
+
+        //Make the square darker ever time you hover over it
     }
 });
 
+
+//Allowing Users to Specify Dimension
+
+const dimButton = document.querySelector(".dimension");
+
+dimButton.addEventListener("click", (e) => {
+
+    //Promt the user for input size and save it to a variable
+    size = prompt("What Size would you like the grid to be?: ");
+
+    //Delete the current grid
+    deleteGrid(containerDiv);
+
+    //Render the new grid
+    createGrid(size);
+})
 
